a4255fce27601fcd51050c1cd862c1f641fb738e
Evilcrow <Evilcrow486@gmail.com>
2017-11-15 20:30:34 +0800

    Update: This is my own Molokai ,very Cool!

---
+++ 1.c
@@ -0,0 +1,14 @@
+/*
+ * @filename:    1.c
+ * @author:      Crow
+ * @date:        11/15/2017 20:12:53
+ * @description:
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/init.h>
+
+
